d58c652912cba2e7cf387671fac49e0c4c4d72dd
brookslangley <brookslangley@brookss-air.lan>
2019-09-01 23:54:41 -0400

    first novice commit a1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,34 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		// Total number of customers
+		
+		int count = scan.nextInt();
+		
+		//Array to store orders
+		
+		String[] orders = new String[count];
+		
+
+		for (int i=0; i<orders.length; i++) {
+			String firstnames = scan.next();
+			String lastnames = scan.next();
+			String itemname = "";
+			Double price = 0.0;
+			int items = 0;
+			Double total = 0.0;
+			char initial = firstnames.charAt(0);
+			int number = scan.nextInt();
+				for (int x=0; x<number; x++) {
+					items = scan.nextInt();
+					itemname = scan.next();
+					price = scan.nextDouble();
+					total += items*price;
+		}
+				String quad = String.format("%.2f", total);
+				System.out.println(initial + ". " + lastnames + " " + quad);
+		}
+	scan.close();	
 	}
 }
